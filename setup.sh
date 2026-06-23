6980eb17894453c7799b02bc55e1c3abb439d980
EwanMcA <ewan.mcandrew@vanta.com>
2026-06-23 15:23:05 +0100

    dotfiles

--- setup.sh
+++ setup.sh
@@ -7,19 +7,4 @@ if [ -f "$HOME/.zshrc" ]; then
   mv "$HOME/.zshrc" "$HOME/.zshrc-bak"
 fi
 
-stow --target="$HOME" \
-  --ignore='.git' \
-  --ignore='.gitmodules' \
-  --ignore='README.md' \
-  --ignore='setup.sh' \
-  .
-}
-
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  echo "Installing oh-my-zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-else
-  echo "oh-my-zsh already installed"
-fi
-
 sudo chsh -s $(which zsh) $(whoami)
